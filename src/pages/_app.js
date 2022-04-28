aa945d859ba7a53b163f86ee96aacc82c2ff57ed
POOJA RAWAT <poojar@audaxlabs.com>
2022-04-28 15:55:29 +0530

    Some fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -21,39 +21,39 @@ function MyApp({ Component, pageProps }) {
 
     useEffect(() => {
         // on initial load - run auth check
-        // authCheck(router.asPath);
+        authCheck(router.asPath);
 
-        // // on route change start - hide page content by setting authorized to false
-        // const hideContent = () => setAuthorized(false);
-        // router.events.on('routeChangeStart', hideContent);
+        // on route change start - hide page content by setting authorized to false
+        const hideContent = () => setAuthorized(false);
+        router.events.on('routeChangeStart', hideContent);
 
-        // // on route change complete - run auth check
-        // router.events.on('routeChangeComplete', authCheck)
+        // on route change complete - run auth check
+        router.events.on('routeChangeComplete', authCheck)
 
-        // // unsubscribe from events in useEffect return function
-        // return () => {
-        //     router.events.off('routeChangeStart', hideContent);
-        //     router.events.off('routeChangeComplete', authCheck);
-        // }
+        // unsubscribe from events in useEffect return function
+        return () => {
+            router.events.off('routeChangeStart', hideContent);
+            router.events.off('routeChangeComplete', authCheck);
+        }
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     function authCheck(url) {
         // redirect to login page if accessing a private page and not logged in
-        // setUser(userService.userValue);
-        // const publicPaths = ['/login'];
-        // const path = url.split('?')[0];
+        setUser(userService.userValue);
+        const publicPaths = ['/login'];
+        const path = url.split('?')[0];
 
-        // if (!userService?.userValue?.username && !publicPaths.includes(path)) {
-        //     setAuthorized(false);
-        //     router.push({
-        //         pathname: router.asPath === '/' && '/login',
-        //         query: { returnUrl: router.asPath }
-        //     });
-        // } else {
-        //     setAuthorized(true);
-        // }
+        if (!userService?.userValue?.username && !publicPaths.includes(path)) {
+            setAuthorized(false);
+            router.push({
+                pathname: router.asPath === '/' && '/login',
+                query: { returnUrl: router.asPath }
+            });
+        } else {
+            setAuthorized(true);
+        }
     }
 
     return (
@@ -70,9 +70,9 @@ function MyApp({ Component, pageProps }) {
                 src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
                 integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
                 crossorigin="anonymous" />
-            {/* {authorized && */}
-            < Component {...pageProps} />
-            {/* } */}
+            {authorized &&
+                < Component {...pageProps} />
+            }
         </>
     )
 }
