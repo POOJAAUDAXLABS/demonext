25db25601bdf630afbd04078146dc340784d4420
POOJAAUDAXLABS <104560552+POOJAAUDAXLABS@users.noreply.github.com>
2022-05-10 15:03:26 +0530

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,8 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import excuteQuery from "../../lib/db"
-export default async function handler(req, res) {
-    const result = await excuteQuery({ query: "SELECT * from vin WHERE vin = ?", values: [req.query.id] })
-    // query: 'INSERT INTO users (id, createdAt, email, hash, salt) VALUES(?, ?, ?, ?, ?)',
-    // values: [user.id, user.createdAt.toString(), user.email, user.hash, user.salt],
-    res.status(200).json(result)
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
 }
